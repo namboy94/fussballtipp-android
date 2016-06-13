d344eb69b96e6de14c1dd743158d069e3fe640cf
Hermann Krumrey <hermann@krumreyh.com>
2016-06-14 01:10:56 +0200

    Initialized gradle

--- web-api/src/main/java/net/namibsun/johannesbucher/api/Main.java
+++ web-api/src/main/java/net/namibsun/johannesbucher/api/Main.java
@@ -1,7 +1,20 @@
 package net.namibsun.johannesbucher.api;
 
-/**
- * Created by hermann on 6/14/16.
- */
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.HttpURLConnection;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        String url = "https://tippspiel.johannes-bucher.de/";
+        System.out.println(url);
+
+        CookieManager cookieManager = new CookieManager();
+        CookieHandler.setDefault(cookieManager);
+
+
+
+    }
 }
