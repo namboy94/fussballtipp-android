2d2273f7a4deec7de6c38ebf151d08c1a7ef04e5
Hermann Krumrey <hermann@krumreyh.com>
2016-06-14 17:16:46 +0200

    Added license texts.

--- web-api/src/main/java/net/namibsun/johannesbucher/api/utility/UserTableScraper.java
+++ web-api/src/main/java/net/namibsun/johannesbucher/api/utility/UserTableScraper.java
@@ -1,3 +1,25 @@
+/*
+Copyright 2016 Hermann Krumrey
+
+This file is part of johannes-bucher-tippspiel.
+
+    johannes-bucher-tippspiel is a library/program to display and interact with
+    tippspiel.johannes-bucher.de
+
+    johannes-bucher-tippspiel is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    johannes-bucher-tippspiel is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with johannes-bucher-tippspiel. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package net.namibsun.johannesbucher.api.utility;
 
 import net.namibsun.johannesbucher.api.utility.objects.UserTable;
