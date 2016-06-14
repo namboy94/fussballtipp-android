6521a0e76da679d0ffbc5449fa3c2bb85ef29b39
Hermann Krumrey <hermann@krumreyh.com>
2016-06-14 17:02:27 +0200

    Tidied up the code.

--- web-api/src/main/java/net/namibsun/johannesbucher/api/utility/objects/UserTable.java
+++ web-api/src/main/java/net/namibsun/johannesbucher/api/utility/objects/UserTable.java
@@ -2,15 +2,27 @@ package net.namibsun.johannesbucher.api.utility.objects;
 
 import java.util.ArrayList;
 
+/**
+ * Class that holds the user table of the rank.php page
+ */
 public class UserTable {
 
     private ArrayList<UserTableEntry> users = new ArrayList<>();
 
+    /**
+     * Adds a user to the users array list
+     * @param user the user to be added
+     */
     public void addUser(UserTableEntry user) {
         this.users.add(user);
     }
 
+    /**
+     * Turns the UserTable into a string
+     * @return the generated string
+     */
     public String toString() {
+
         String userTable = "";
 
         for (UserTableEntry user: this.users) {
@@ -20,7 +32,7 @@ public class UserTable {
                 previousPositions += prevPos + "-";
             }
 
-            userTable += user.getPosition() + ": " + user.getUsername() + " " + user.getPoints() + " " + previousPositions + "\n";
+            userTable += user.getPosition() + ": " + user.getUsername() + " " + user.getPoints();
         }
 
         return userTable;
--- web-api/src/main/java/net/namibsun/johannesbucher/api/utility/objects/UserTableEntry.java
+++ web-api/src/main/java/net/namibsun/johannesbucher/api/utility/objects/UserTableEntry.java
@@ -1,5 +1,8 @@
 package net.namibsun.johannesbucher.api.utility.objects;
 
+/**
+ * Class that models a user entry on the UserTable
+ */
 public class UserTableEntry {
 
     private String username;
@@ -9,6 +12,15 @@ public class UserTableEntry {
     private float averagePoints;
     private int amountOfBets;
 
+    /**
+     * Constructor that assigns the user information to their respective local variables
+     * @param username the user's name
+     * @param position the user's current position
+     * @param previousPositions the user's placement history
+     * @param points the current amount of points the user has
+     * @param averagePoints the average amount of points the user earned per match
+     * @param amountOfBets the total amount of bets the user made
+     */
     public UserTableEntry(String username,
                           int position, int[] previousPositions, int points, float averagePoints, int amountOfBets) {
         this.username = username;
@@ -19,26 +31,44 @@ public class UserTableEntry {
         this.amountOfBets = amountOfBets;
     }
 
+    /**
+     * @return the username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * @return the current position in the table
+     */
     public int getPosition() {
         return position;
     }
 
+    /**
+     * @return the position history of the user
+     */
     public int[] getPreviousPositions() {
         return previousPositions;
     }
 
+    /**
+     * @return the current amount of points
+     */
     public int getPoints() {
         return points;
     }
 
+    /**
+     * @return the average amount of points per match
+     */
     public float getAveragePoints() {
         return averagePoints;
     }
 
+    /**
+     * @return the amount of bets the user has made so far
+     */
     public int getAmountOfBets() {
         return amountOfBets;
     }
